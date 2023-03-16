a633911dbd9359d7acb883338782d59619bf4608
Justin Farnsworth <farnswj1@tcnj.edu>
2023-03-15 22:43:23 -0400

    refactoring and utilized docstrings

--- unix_epoch_end_countdown.py
+++ unix_epoch_end_countdown.py
@@ -33,17 +33,17 @@ class TimeArray(NamedTuple):
 
 
 class UnixEpochEndCountdown:
-    # Constructor
     def __init__(self):
-        # Initializes pygame and sets the name of window
+        """Constructor"""
+        # Initialize pygame and sets the name of window
         pygame.init()
         pygame.display.set_caption("Unix Epoch End Countdown")
 
-        # Sets the window dimensions and clock
+        # Set the window dimensions and clock
         self.__window = pygame.display.set_mode((800, 200))
         self.__clock = pygame.time.Clock()
 
-        # Initializes the font for the HUD
+        # Initialize the font for the HUD
         self.__primary_font = pygame.font.Font("data/fonts/digital-7 mono.ttf", 100)
 
         # Music (all songs in this list are made my Pogo)
@@ -52,30 +52,27 @@ class UnixEpochEndCountdown:
             song for song in listdir(self.__music_dir) if song.endswith(".ogg")
         )
 
-        # A song will be randomly chosen from the musicboard and loaded.
+        # A song will be randomly chosen from the musicboard and loaded
         pygame.mixer.music.set_volume(1)
         pygame.mixer.music.load(self.__music_dir + choice(self.__musicboard))
 
-        # Set the initial time difference.
-        delta = 2147483648 - int(time.time())
-        self.__time_array = self.__convert_seconds_to_time(delta)
+        # Set the initial time difference
+        self.update_timer()
 
-    # Main loop
     def run(self):
-        # Initially, a song will be ready to play.
+        """Run the main loop."""
+        # Initially, a song will be ready to play
         next_song_ready = True
 
-        # Closing the window or clicking the ESC key terminates the program.
+        # Closing the window or clicking the ESC key terminates the program
         while not self.__user_terminates_program():
-            # Sets the FPS to 30
+            # Set the FPS to 30
             self.__clock.tick(30)
 
-            # Converts the difference between the end of the Unix 32-bit timestamp (2 ** 31)
-            # and the current time into days, hours, minutes, and seconds.
-            delta = 2147483648 - int(time.time())
-            self.__time_array = self.__convert_seconds_to_time(delta)
+            # Update the timer
+            self.update_timer()
 
-            # A song will be randomly chosen and played every 5 minutes.
+            # A song will be randomly chosen and played every 5 minutes
             if self.__time_array.minutes % 5 == 0:
                 if self.__time_array.seconds == 0:
                     if next_song_ready:
@@ -85,66 +82,69 @@ class UnixEpochEndCountdown:
                     pygame.mixer.music.load(self.__music_dir + choice(self.__musicboard))
                     next_song_ready = True
 
-            # Draws the timer
+            # Draw the timer
             self.__draw_timer()
 
-    # Draws the timer
     def __draw_timer(self):
-        # Clears the window with a grey canvas
+        """Draw the timer."""
+        # Clear the window with a grey canvas
         self.__window.fill(BLACK)
 
-        # Renders the timer
+        # Render the timer
         time_string_days = self.__primary_font.render(f"{self.__time_array[0]:04}", True, RED)
         time_string_hours = self.__primary_font.render(f"{self.__time_array[1]:02}", True, RED)
         time_string_minutes = self.__primary_font.render(f"{self.__time_array[2]:02}", True, RED)
         time_string_seconds = self.__primary_font.render(f"{self.__time_array[3]:02}", True, RED)
 
-        # This variable will be used to help vertically position the timer on the window.
+        # This variable will be used to help vertically position the timer on the window
         timer_height = 185 - time_string_days.get_height()
 
-        # Draws the timer
+        # Draw the timer
         self.__window.blit(time_string_days, (242 - time_string_days.get_width(), timer_height))
         self.__window.blit(time_string_hours, (310, timer_height))
         self.__window.blit(time_string_minutes, (480, timer_height))
         self.__window.blit(time_string_seconds, (650, timer_height))
 
-        # Draws the colons if permitted.
+        # Draw the colons if permitted
         if self.__time_array[3] % 2 == 0:
             colon = self.__primary_font.render(":", True, RED)
             self.__window.blit(colon, ((552 - colon.get_width()) // 2, timer_height))
             self.__window.blit(colon, ((882 - colon.get_width()) // 2, timer_height))
             self.__window.blit(colon, ((1222 - colon.get_width()) // 2, timer_height))
 
-        # Displays text and a line that divides the text and timer
+        # Display text and a line that divides the text and timer
         title = self.__primary_font.render("The Epochalypse", True, RED)
         self.__window.blit(title, ((800 - title.get_width()) // 2, 10))
         pygame.draw.line(self.__window, RED, (0, 100), (800, 100), 5)
 
-        # Updates the window
+        # Update the window
         pygame.display.update()
 
-    # Checks if the user presses the escape key or closes the window
     @staticmethod
     def __user_terminates_program():
-        to_be_terminated = False
-
+        """Check if the user presses the escape key or closes the window."""
         for event in pygame.event.get():
             # Pressing the ESC key or closing the window terminates the program
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-                to_be_terminated = True
+                return True
 
-        return to_be_terminated
+        return False
 
-    # The returned array contains the numbers in order of total days, hours, minutes, and seconds.
+    def update_timer(self):
+        """Update the timer."""
+        delta = 2147483648 - int(time.time())
+        self.__time_array = self.__convert_seconds_to_time(delta)
+
     @staticmethod
     def __convert_seconds_to_time(seconds: int):
+        """Convert the time in seconds to days, hours, minutes, and seconds."""
         days, r1 = divmod(seconds, 86400)
         hours, r2 = divmod(r1, 3600)
         minutes, _seconds = divmod(r2, 60)
         return TimeArray(days, hours, minutes, _seconds)
 
 
-# Executes the program.
+# Execute the program
 if __name__ == "__main__":
     app = UnixEpochEndCountdown()
     app.run()
